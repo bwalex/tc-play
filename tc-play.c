72a5221205d6a42f1ff5c6747a4b3e0eee1312e0
Alex Hornung <alex@alexhornung.com>
2011-05-16 20:22:27 +0100

    change copyright

--- tc-play.c
+++ tc-play.c
@@ -1,8 +1,5 @@
 /*
- * Copyright (c) 2011 The DragonFly Project.  All rights reserved.
- *
- * This code is derived from software contributed to The DragonFly Project
- * by Alex Hornung <ahornung@gmail.com>
+ * Copyright (c) 2011 Alex Hornung <alex@alexhornung.com>. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
