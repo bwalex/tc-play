c4b5f047f17488e196c4e56c51a8adf58378d6a0
a1346054 <36859588+a1346054@users.noreply.github.com>
2021-09-01 21:33:53 +0000

    fix shellcheck warnings

--- test/reduce_test_vol.sh
+++ test/reduce_test_vol.sh
@@ -19,29 +19,27 @@ SRC_VOL=$1
 DST_VOL=$2
 
 # Find the total size of the source volume, in bytes.
-SZ=`stat -c "%s" $SRC_VOL`
+SZ=`stat -c "%s" "$SRC_VOL"`
 
 # Find the total size of the source volume, in blocks.
-SZ_BLOCKS=`echo $SZ / 512 | bc`
+SZ_BLOCKS=`echo "$SZ / 512" | bc`
 
 # Find the block at which the backup header area starts.
-BCK_HDR_START=`echo $SZ_BLOCKS - 256 | bc`
+BCK_HDR_START=`echo "$SZ_BLOCKS - 256" | bc`
 
 # Define the sizes of the header and backup header areas,
 # in blocks.
 HDR_AREA_BLOCKS=256
 BCK_HDR_AREA_BLOCKS=256
 
-
 # Create new zero-filled volume with the same size as the
 # source volume.
-dd if=/dev/zero of=$DST_VOL bs=512 count=$SZ_BLOCKS
+dd if=/dev/zero of="$DST_VOL" bs=512 count="$SZ_BLOCKS"
 
 # Copy over the header area.
-dd if=$SRC_VOL  of=$DST_VOL bs=512 count=$HDR_AREA_BLOCKS       \
+dd if="$SRC_VOL" of="$DST_VOL" bs=512 count="$HDR_AREA_BLOCKS"     \
     conv=notrunc
 
 # Copy over the backup header area.
-dd if=$SRC_VOL  of=$DST_VOL bs=512 count=$BCK_HDR_AREA_BLOCKS   \
-   conv=notrunc seek=$BCK_HDR_START skip=$BCK_HDR_START
-
+dd if="$SRC_VOL" of="$DST_VOL" bs=512 count="$BCK_HDR_AREA_BLOCKS" \
+    conv=notrunc seek="$BCK_HDR_START" skip="$BCK_HDR_START"
